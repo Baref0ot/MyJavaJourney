83cf196591c630b3be41aa8fc7a9071e6ed107d4
Matthew Wright <Mattwright700@yahoo.com>
2018-12-04 20:07:05 -0500

    Add files via upload

---
+++ Names.java
@@ -0,0 +1,16 @@
+/************************************************************************
+  Matthew Lee Wright
+  Ron Enz (In Class- JavaI)
+  Janurary 24, 2018
+*************************************************************************/
+
+public class Names{
+	public static void main(String[] args)
+	{
+		//Declarations
+		String firstName = "Matthew";
+		String lastName = "Wright";
+	  System.out.println(firstName);
+	  System.out.println(lastName);
+	}
+}
